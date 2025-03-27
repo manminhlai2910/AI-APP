25a94efc1d4f67fc5ba5a5fb73ca59af9e04e906
Man Lai <manlai@Mans-MacBook-Air.local>
2025-03-27 10:10:24 -0400

    change firebase config

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: Request) {
   });
 
   try {
-    const { text: questions } = await ai.models.ai.models.generateContent({
+    const { text: questions } = await ai.models.generateContent({
       model: "gemini-2.0-flash-001",
       contents: `Prepare questions for a job interview.
         The job role is ${role}.
